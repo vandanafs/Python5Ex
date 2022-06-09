846abb21e7f643dd65a446ecf825f3c304bb75e3
vandanafs <vandana.f.09@gmail.com>
2022-06-09 13:03:06 -0400

    adding chnages

--- anagram.py
+++ anagram.py
@@ -2,5 +2,11 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    pass  # remove pass statement and implement me
+   # pass  # remove pass statement and implement me
+    if sorted(first_string) ==  sorted(second_string) :
+        return True
+    else:
+         return False
+
+
 
